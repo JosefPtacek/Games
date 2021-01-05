7980ceb85b193102eaf1e5f0c5acd685a8eb92bc
josefPtacek <josefptacek87@seznam.cz>
2021-01-05 19:58:29 +0100

    had se pohybuje

--- js/main.js
+++ js/main.js
@@ -5,5 +5,19 @@ var snakeSize = 50;
 var snakePosX = 0;
 var snakePosY = canvas.width / 2;
 
-kontext.fillStyle = "black";
-kontext.fillRect(snakePosX, snakePosY, snakeSize, snakeSize);
+var snakeSpeed = 50;
+var fsp = 8;
+
+function gameLoop() {
+    kontext.fillStyle = "white"
+    kontext.fillRect(0, 0, canvas.width, canvas.height);
+
+    kontext.fillStyle = "black";
+    kontext.fillRect(snakePosX, snakePosY, snakeSize, snakeSize);
+
+    snakePosX += snakeSpeed;
+
+    setTimeout(gameLoop, 1000/fsp);
+}
+
+gameLoop();
