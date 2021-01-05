9753fea94584fce1511eaad056478ad63da8cf54
josefPtacek <josefptacek87@seznam.cz>
2021-01-05 20:44:25 +0100

    ovládání hada pomocí šipek

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// událost
+document.addEventListener("keydown", keyPush);
+// canvas
 var canvas = document.querySelector("canvas");
 var kontext = canvas.getContext("2d");
 
+// player
 var snakeSize = 50;
 var snakePosX = 0;
 var snakePosY = canvas.width / 2;
@@ -8,6 +12,10 @@ var snakePosY = canvas.width / 2;
 var snakeSpeed = 50;
 var fsp = 8;
 
+var velocityPosX = 1; 
+var velocityPosY = 0;
+
+// spuštění hry
 function gameLoop() {
     drawStuff();
     moveStuff();
@@ -28,9 +36,41 @@ function drawStuff() {
 
 // pohyb
 function moveStuff() {
-    snakePosX += snakeSpeed;
+    snakePosX += snakeSpeed * velocityPosX;
+    snakePosY += snakeSpeed * velocityPosY;
 
+    // kolize stěny s hadem
     if(snakePosX > canvas.width) {
         snakePosX = 0;
     }
 };
+
+// pohyb klávesnicí
+function keyPush(event) {
+    switch(event.key) {
+        case "ArrowUp":
+            if(velocityPosY !== 1) {
+                velocityPosX = 0;
+                velocityPosY = -1;
+            }
+            break;
+        case "ArrowDown":
+            if(velocityPosY !== -1) {
+                velocityPosX = 0;
+                velocityPosY = 1;
+            }
+            break;
+        case "ArrowLeft":
+            if(velocityPosX !== 1) {
+                velocityPosX = -1;
+                velocityPosY = 0;
+            }
+            break;
+        case "ArrowRight":
+            if(velocityPosX !== -1) {
+                velocityPosX = 1;
+                velocityPosY = 0;
+            }
+            break;
+    }
+}
