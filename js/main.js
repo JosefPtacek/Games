663edb43ccac356bc8d6295aa3b69e3bb89971cd
josefPtacek <josefptacek87@seznam.cz>
2021-01-05 19:33:17 +0100

    vytvořil jsem hlavu hada

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 var canvas = document.querySelector("canvas");
+var kontext = canvas.getContext("2d");
+
+var snakeSize = 50;
+var snakePosX = 0;
+var snakePosY = canvas.width / 2;
+
+kontext.fillStyle = "black";
+kontext.fillRect(snakePosX, snakePosY, snakeSize, snakeSize);
